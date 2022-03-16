3aaccc2cdb561d661554e171e23b9cf510b645fd
alick <76238763+vilaysone01@users.noreply.github.com>
2022-03-16 15:50:42 +0700

    publication

--- controller/tb_publication.controller.js
+++ controller/tb_publication.controller.js
@@ -5,6 +5,7 @@ const fs = require('fs-extra');
 
 
 exports.insert= async (req, res, next)=>{
+   try {
     const form = new formidable.IncomingForm()
     form.parse(req, async (error, fields, files) => {
         const result = await models.tb_plublication.create({
@@ -26,21 +27,40 @@ exports.insert= async (req, res, next)=>{
 
         return res.status(201).json({ message: "success" })
     })
+   } catch (error) {
+       res.status(500).json({
+           error:error.message
+       })
+   }
 }
    
       
 
 
 // show data lao content
-exports.index= async(req, res)=>{
-    res.json({})
+exports.index_la= async(req, res)=>{
+    const pub_la = await models.tb_plublication.findAll({
+        attributes:['lao_topic','lao_Content','createdAt']
+    })
+    res.json({
+        data:pub_la
+    })
+}
+// show data eng content
+exports.index_en= async(req, res)=>{
+    const pub_en = await models.tb_plublication.findAll({
+        attributes:['eng_topic','eng_Content','createdAt']
+    })
+    res.json({
+        data:pub_en
+    })
 }
 
 // update
 exports.update = async (req, res)=>{
     try {
-        const {id} = req.parse;
-    const pub = await model.tb_plublication.findByPk(id);
+        const {id} = req.params;
+    const pub = await models.tb_plublication.findByPk(id);
     if(!pub){
         res.status(404).json({
             message:"Not Found!"
@@ -48,7 +68,7 @@ exports.update = async (req, res)=>{
     }else{
         const form = new formidable.IncomingForm()
     form.parse(req, async (error, fields, files) => {
-        const result = await models.tb_plublication.create({
+        const result = await models.tb_plublication.update({
             no: fields.no,
             lao_topic: fields.lao_topic,
             lao_Content: fields.lao_Content,
@@ -56,9 +76,8 @@ exports.update = async (req, res)=>{
             eng_topic: fields.eng_topic
         }, { where:{id:id},logging: false })
         _path = "/pub"
-        _lao_Content = await upload.uploadFile(files.lao_Content, result.lao_Content, result.id, _path, "la");
-        console.log(_lao_Content)
-        _eng_Content= await upload.uploadFile(files.eng_Content, result.eng_Content, result.id, _path, "en");
+        _lao_Content = await upload.uploadFile(files.lao_Content, result.lao_Content, id, _path, "la");
+        _eng_Content= await upload.uploadFile(files.eng_Content, result.eng_Content, id, _path, "en");
 
         await models.tb_plublication.update({
             lao_Content: _lao_Content,
@@ -77,102 +96,32 @@ exports.update = async (req, res)=>{
     }
 }
 
-// // show lao content
-// exports.index_la= async(req, res)=>{
-//    const pub = await model.tb_plublication.findAll({
-//     attributes:['lao_topic','lao_content','createdAt']
-//    });
+// delete
+exports.delete = async (req, res)=>{
+    try {
+        const {id} = req.params;
+    const pub = await models.tb_plublication.findByPk(id);
+    if(!id){
+        res.status(404).json({
+            message:'not Found ID!'
+        })
+    }else{
+        const result = await models.tb_plublication.findOne({where:{id}});
+        _path="public/images/pub/"
+        upload.deleteFile(_path, result.lao_Content)
+        upload.deleteFile(_path, result.eng_Content)
+        await models.tb_plublication.destroy({where:{id:id}, logging:false});
+        res.status(200).json({
+            message:"deleted success"
+        })
+    }
+    } catch (error) {
+        res.status(400).json({
+            error:error.message
+        })
+    }
+}
 
-//   try {
-//     res.status(200).json({
-        
-//         data:pub,
-        
-//     })
-//   } catch (error) {
-//       res.status(400).json({
-//           error:true,
-//           message:error.message
-//       })
-//   }
-// }
-// // show eng content
-// exports.index_en= async(req, res)=>{
-//     const pub = await model.tb_plublication.findAll({
-//      attributes:['eng_topic','eng_content','createdAt']
-//     });
- 
-//    try {
-//      res.status(200).json({
-         
-//          data:pub,
-         
-//      })
-//    } catch (error) {
-//        res.status(400).json({
-//            error:true,
-//            message:error.message
-//        })
-//    }
-//  }
-// // detele
-// exports.delete = async(req, res) =>{
-//     try {
-//         const {id} = req.params;
 
-//    if(!id){
-//     res.status(400).json({
-//         error:true,
-//         message:'Not found ID!',
-//         data:null
-//     })
-//    }else{
-//     const pub = await model.tb_plublication.destroy({
-//         where:{id:id}
-//     });
-
-//     res.status(200).json({
-//         data:pub,
-//         message:'data updated!'
-//     })
-//    }
-//     } catch (error) {
-//         res.status(400).json({
-//             data:error
-//         })
-//     }
-// }
-
-// // update
-
-// exports.update =async (req, res)=>{
-//     try {
-//         const {id} = req.params;
-//         const {no, lao_topic,eng_topic,lao_Content, eng_Content} = req.body;
-//     if(!id){
-        
-//         res.status(400).json({
-//             error:true,
-//             message:'Not found ID!'
-//         })
-//     }else{
-//         const pub = await model.tb_plublication.update({
-//             no:no,
-//             lao_topic:lao_topic,
-//             eng_topic:eng_topic,
-//             lao_Content:lao_Content,
-//             eng_Content:eng_Content
-//         }, {where:{id:id}})
-    
-//         res.status(200).json({
-//             data:pub,
-//             message:'data updated!'
-//         });
-//     }
-//     } catch (error) {
-//         console.log(error)
-//     }
-
-// }
 
 
--- routes/publication.js
+++ routes/publication.js
@@ -4,6 +4,9 @@ const router = express.Router();
 
 router.post('/',controller.insert);
 router.put('/:id',controller.update);
+router.delete('/:id',controller.delete);
+router.get('/la',controller.index_la);
+router.get('/en',controller.index_en);
 
 
 module.exports=router;
